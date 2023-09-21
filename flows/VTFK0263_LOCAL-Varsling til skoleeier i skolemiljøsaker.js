fcf6702af656524ad3ebb140664097922af4cdad
Robin Ellingsen <robin-elli@hotmail.com>
2023-09-21 14:52:17 +0200

    standard

--- flows/VTFK0263_LOCAL-Varsling til skoleeier i skolemiljøsaker.js
+++ flows/VTFK0263_LOCAL-Varsling til skoleeier i skolemiljøsaker.js
@@ -22,28 +22,28 @@ module.exports = {
             prodSiteId: '427960b4-519d-45da-970a-869c3a88d1b4',
             prodPath: 'sites/OF-Pedagogisksttteogutvikling/Lists/Varsling til skoleeier i skolemiljsaker/AllItems.aspx',
             prodListId: 'b257290a-2a26-4b11-9356-7c36d697ea76',
-            uploadFormPdf: true, // Spørmsål til Nils, Laster denne opp formen til sharepoint listen? 
+            uploadFormPdf: true, // Spørmsål til Nils, Laster denne opp formen til sharepoint listen?
             uploadFormAttachments: false,
-            fields : {
-                Title:	`Sak meldt inn av: ${xmlData.personNavn}` || 'Mangler title', // husk å bruke internal name på kolonnen
-                Hastersaken_x003f_:	xmlData.hasterSaken || ' ',
-                Oppf_x00f8_lging:	xmlData.oppfolging || ' ',
-                Sakstype:	xmlData.saksType || ' ',
-                Datoforvarsling:	xmlData.dato || ' ',
-                Saksnummer:	xmlData.saksNummer || ' ',
-                Skole:	xmlData.personSkole || ' ',
-                Kontaktperson:	xmlData.kontaktpersonNavn || ' ',
-                Kontaktpersontelefonnummer:	xmlData.kontaktPersonTlf || ' ',
-                Vold:	xmlData.saksTypeVold || 'Nei',
-                Digitalmobbing:	xmlData.saksTypeDigitalMobbing || 'Nei',
-                P_x00e5_g_x00e5_ttoverlangtid:	xmlData.saksTypeLangTid || 'Nei',
-                Trusler:	xmlData.saksTypeTrusler || 'Nei',
-                Annet:	xmlData.saksTypeAnnet || 'Nei',
-                EnkeltelevJente:	xmlData.hvemGjelderSakenJente || ' ',
-                EnkeltelevGutt:	xmlData.hvemGjelderSakenGutt || ' ',
-                Klasse:	xmlData.hvemGjelderSakenKlasse || ' ',
-                Meldtinnav:	xmlData.personEpost || ' ',
-                Er_x0020_det_x0020_registrert_x0:	xmlData.tqm || ' ',
+            fields: {
+              Title: `Sak meldt inn av: ${xmlData.personNavn}` || 'Mangler title', // husk å bruke internal name på kolonnen
+              Hastersaken_x003f_: xmlData.hasterSaken || ' ',
+              Oppf_x00f8_lging: xmlData.oppfolging || ' ',
+              Sakstype: xmlData.saksType || ' ',
+              Datoforvarsling: xmlData.dato || ' ',
+              Saksnummer: xmlData.saksNummer || ' ',
+              Skole: xmlData.personSkole || ' ',
+              Kontaktperson: xmlData.kontaktpersonNavn || ' ',
+              Kontaktpersontelefonnummer: xmlData.kontaktPersonTlf || ' ',
+              Vold: xmlData.saksTypeVold || 'Nei',
+              Digitalmobbing: xmlData.saksTypeDigitalMobbing || 'Nei',
+              P_x00e5_g_x00e5_ttoverlangtid: xmlData.saksTypeLangTid || 'Nei',
+              Trusler: xmlData.saksTypeTrusler || 'Nei',
+              Annet: xmlData.saksTypeAnnet || 'Nei',
+              EnkeltelevJente: xmlData.hvemGjelderSakenJente || ' ',
+              EnkeltelevGutt: xmlData.hvemGjelderSakenGutt || ' ',
+              Klasse: xmlData.hvemGjelderSakenKlasse || ' ',
+              Meldtinnav: xmlData.personEpost || ' ',
+              Er_x0020_det_x0020_registrert_x0: xmlData.tqm || ' '
             }
           }
         ]
@@ -63,7 +63,7 @@ module.exports = {
           description,
           type: 'Varsling til skoleeier i skolemiljøsaker', // Required. A short searchable type-name that distinguishes the statistic element
           // optional fields:
-          Sakstype:	xmlData.saksType
+          Sakstype: xmlData.saksType
         }
       }
     }

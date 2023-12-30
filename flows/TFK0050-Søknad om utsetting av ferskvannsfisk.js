e6fc69a456d379574791f94295910be40e6588c3
Robin Ellingsen <robin-elli@hotmail.com>
2023-12-30 11:02:12 +0100

    1.0.2

---
+++ flows/TFK0050-Søknad om utsetting av ferskvannsfisk.js
@@ -0,0 +1,158 @@
+const description = 'Søknad om utsetting av ferskvannsfisk'
+const { nodeEnv } = require('../config')
+
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseXml: {
+    enabled: true
+  },
+
+  /* XML from Acos:
+ArchiveData {
+string fnr
+string Fornavn
+string Etternavn
+string Adresse
+string Postnr
+string Poststed
+string Mobilnr
+string Epost
+}
+
+  */
+  syncPrivatePerson: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr
+        }
+      }
+    }
+  },
+  handleCase: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        return {
+          service: 'CaseService',
+          method: 'CreateCase',
+          parameter: {
+            CaseType: 'Sak',
+            Project: nodeEnv === 'production' ? '' : '23-18', // Må lages nytt prosjekt for Prod i 2024,
+            Title: `Søknad om utsetting av fisk i ${flowStatus.parseXml.result.ArchiveData.Egendefinert1} - ${flowStatus.parseXml.result.ArchiveData.Kommune} kommune`,
+            // UnofficialTitle: ,
+            Status: 'B',
+            JournalUnit: 'Sentralarkiv',
+            SubArchive: 'Sakarkiv',
+            ArchiveCodes: [
+              {
+                ArchiveCode: '---',
+                ArchiveType: 'FELLESKLASSE PRINSIPP',
+                Sort: 1
+              },
+              {
+                ArchiveCode: 'K62',
+                ArchiveType: 'FAGKLASSE PRINSIPP',
+                Sort: 2
+              }
+            ],
+            // ResponsibleEnterpriseNumber: '45678912',
+            ResponsiblePersonEmail: 'jamila.synnove.saber@telemarkfylke.no',
+            AccessGroup: 'Alle'
+          }
+        }
+      }
+    }
+  },
+  // Arkiverer dokumentet i 360
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const caseNumber = flowStatus.handleCase.result.CaseNumber
+        // const caseNumber = nodeEnv === 'production' ? 'må fylles inn!' : '23/00115'
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: xmlData.Fnr,
+                Role: 'Avsender',
+                IsUnofficial: false
+              }
+            ],
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Søknad om utsetting av fisk',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Status: 'J',
+            DocumentDate: new Date().toISOString(),
+            Title: `Søknad om utsetting av fisk - ${xmlData.Kommune}`,
+            UnofficialTitle: `Søknad om utsetting av fisk - ${xmlData.Egendefinert1} - ${xmlData.Kommune}`,
+            Archive: 'Saksdokument',
+            CaseNumber: caseNumber,
+            // ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200025' : '200031', // Seksjon Kultur Dette finner du i p360, ved å trykke "Avansert Søk" > "Kontakt" > "Utvidet Søk" > så søker du etter det du trenger Eks: "Søkenavn": %Idrett%. Trykk på kontakten og se etter org nummer.
+            ResponsiblePersonEmail: 'jamila.synnove.saber@telemarkfylke.no',
+            AccessCode: '7',
+            Paragraph: 'Offl. § 7d',
+            AccessGroup: 'Klima og miljø'
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Samfunnsutvikling',
+          department: 'Klima og miljø',
+          description, // Required. A description of what the statistic element represents
+          type: 'Utsetting av ferskvannsfisk', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          documentNumber: flowStatus.archive.result.DocumentNumber, // Optional. anything you like
+          kommune: flowStatus.parseXml.result.ArchiveData.Kommune
+        }
+      }
+    }
+  },
+  failOnPurpose: {
+    enabled: false
+  }
+}

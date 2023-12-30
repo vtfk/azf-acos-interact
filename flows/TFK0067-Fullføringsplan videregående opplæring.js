2b349ccf3e5c261b6b27b8d3893c885b17245b4c
Robin Ellingsen <robin-elli@hotmail.com>
2023-12-30 10:49:55 +0100

    Skjema i prod

---
+++ flows/TFK0067-Fullføringsplan videregående opplæring.js
@@ -0,0 +1,185 @@
+const description = 'Sender til elevmappe'
+// const { nodeEnv } = require('../config')
+const { schoolInfo } = require('../lib/data-sources/tfk-schools')
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseXml: {
+    enabled: true,
+    options: {
+    }
+  },
+  /* Felter fra Acos:
+    ArchiveData {
+      string Fnr
+      string Fornavn
+      string Etternavn
+      string Adresse
+      string Postnr
+      string Poststed
+      string Mobilnr
+      string Epost
+      string AnsVirksomhet
+      string AnsEpost
+      string Tilgangsgruppe
+      string Tittel
+      string FiktivtFnr
+      string SkoleOrgNr
+      string Egendefinert1
+      string Egendefinert2
+      string Egendefinert3
+}
+  */
+
+  // Synkroniser elevmappe
+  syncElevmappe: {
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus) => { // for å opprette person basert på fødselsnummer
+        // Mapping av verdier fra XML-avleveringsfil fra Acos.
+        return {
+          ssn: flowStatus.parseXml.result.ArchiveData.Fnr
+        }
+      }
+    }
+  },
+
+  // Arkiverer dokumentet i elevmappa
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      /*
+      condition: (flowStatus) => { // use this if you only need to archive some of the forms.
+        return flowStatus.parseXml.result.ArchiveData.TilArkiv === 'true'
+      },
+      */
+      mapper: (flowStatus, base64, attachments) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const elevmappe = flowStatus.syncElevmappe.result.elevmappe
+        const school = schoolInfo.find(school => school.orgNr.toString() === xmlData.SkoleOrgNr)
+        if (!school) throw new Error(`Could not find any school with orgNr: ${xmlData.SkoleOrgNr}`)
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            AccessCode: '13',
+            AccessGroup: school.tilgangsgruppe,
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: xmlData.Fnr,
+                Role: 'Avsender',
+                IsUnofficial: true
+              }
+            ],
+            DocumentDate: new Date().toISOString(),
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: 'Fullføringsplan videregående opplæring',
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Paragraph: 'Offl. § 13 jf. fvl. § 13 (1) nr.1',
+            ResponsibleEnterpriseNumber: xmlData.SkoleOrgNr,
+            // ResponsiblePersonEmail: '',
+            Status: 'J',
+            Title: 'Fullføringsplan videregående opplæring',
+            UnofficialTitle: 'Fullføringsplan videregående opplæring',
+            Archive: 'Sensitivt elevdokument',
+            CaseNumber: elevmappe.CaseNumber
+          }
+        }
+      }
+    }
+
+  },
+
+  signOff: {
+    enabled: false
+  },
+
+  closeCase: {
+    enabled: false
+  },
+  /*
+    sharepointList: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        if (!xmlData.Postnr) throw new Error('Postnr har ikke kommet med fra XML')
+        return [
+          {
+            siteId: '0a4121ce-7384-474c-afff-ee20f48bff5e',
+            path: 'sites/BDK-Jrgensteste-team/Lists/ACOS%20test%20%20Bestilling%20av%20dokumentasjon%20for%20privati/AllItems.aspx',
+            listId: 'D1085908-9111-4b6d-84d3-fc8ecd29d398',
+            uploadFormPdf: true,
+            uploadFormAttachments: true,
+            fields: {
+              Title: xmlData.Fnr || 'Mangler fnr', // husk å bruke internal name på kolonnen
+              Fornavn: xmlData.Fornavn,
+              Etternavn: xmlData.Etternavn,
+              Adresse: xmlData.Adresse,
+              Postnummerogsted: `${xmlData.Postnr} ${xmlData.Poststed}`,
+              Mobil: xmlData.Mobil,
+              E_x002d_postadresse: xmlData.Epost,
+              Typedokumentasjon: xmlData.TypeDok,
+              Typeautorasisjon: xmlData.TypeAut,
+              Eksamenssted: xmlData.Eksamenssted,
+              Fag: xmlData.Fag,
+              _x00d8_nsketmottak: xmlData.OnsketMottak,
+              Eksamensperiode: xmlData.AarSemester,
+              Alternativadresse: xmlData.AltAdresse
+
+            }
+          }
+        ]
+      }
+    }
+  },
+  */
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // const xmlData = flowStatus.parseXml.result.ArchiveData
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Opplæring',
+          department: '',
+          description,
+          type: 'Fullføringsplan videregående opplæring', // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+          tilArkiv: flowStatus.parseXml.result.ArchiveData.TilArkiv,
+          documentNumber: flowStatus.archive?.result?.DocumentNumber || 'tilArkiv er false' // Optional. anything you like
+        }
+      }
+    }
+  },
+
+  failOnPurpose: {
+    enabled: false
+  }
+}

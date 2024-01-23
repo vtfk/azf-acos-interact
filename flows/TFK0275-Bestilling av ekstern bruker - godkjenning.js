6075a62f769fac3984ada51fc1f7add5167cff05
Robin Ellingsen <robin-elli@hotmail.com>
2024-01-23 13:09:57 +0100

    Ekstern bruker

---
+++ flows/TFK0275-Bestilling av ekstern bruker - godkjenning.js
@@ -0,0 +1,15 @@
+// Eksempel på lokal avlevering av filer
+module.exports = {
+  config: {
+    enabled: true,
+    doNotRemoveBlobs: false
+  },
+  parseXml: {
+    enabled: true,
+    options: {
+    }
+  },
+  groundControl: {
+    enabled: true // Files will be copied to GROUND_CONTROL_STORAGE_ACCOUNT_CONTAINER_NAME, and will be downloaded on local server (./ground-control/index.js)
+  }
+}

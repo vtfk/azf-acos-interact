b97f5dddca1e495f4a20c70906c6689db87ef0b3
Robin Ellingsen <robin-elli@hotmail.com>
2024-01-26 15:30:32 +0100

    spaserstokken

--- flows/TFK0078-Rapporteringsskjema - Den kulturelle spaserstokken.js
+++ flows/TFK0078-Rapporteringsskjema - Den kulturelle spaserstokken.js
@@ -1,11 +1,111 @@
+const description = 'Den kulturelle spaserstokken'
+const { nodeEnv } = require('../config')
+
 module.exports = {
   config: {
-    enabled: false,
+    enabled: true,
     doNotRemoveBlobs: false
   },
   parseXml: {
+    enabled: true
+  },
+
+  /* XML from Acos:
+ArchiveData {
+string Egendefinert1 // OrgNr
+string Egendefinert2 // OrgNavn
+}
+
+  */
+  syncEnterprise: {
     enabled: true,
     options: {
+      mapper: (flowStatus) => { // for å opprette organisasjon basert på orgnummer
+        return {
+          orgnr: flowStatus.parseXml.result.ArchiveData.Egendefinert1.replaceAll(' ', '')
+        }
+      }
     }
+  },
+
+  // Arkiverer dokumentet i 360
+  archive: { // archive må kjøres for å kunne kjøre signOff (noe annet gir ikke mening)
+    enabled: true,
+    options: {
+      mapper: (flowStatus, base64, attachments) => {
+        const xmlData = flowStatus.parseXml.result.ArchiveData
+        const caseNumber = nodeEnv === 'production' ? '24/04354' : '24/00062'
+        const p360Attachments = attachments.map(att => {
+          return {
+            Base64Data: att.base64,
+            Format: att.format,
+            Status: 'F',
+            Title: att.title,
+            VersionFormat: att.versionFormat
+          }
+        })
+        return {
+
+          service: 'DocumentService',
+          method: 'CreateDocument',
+          parameter: {
+            Category: 'Dokument inn',
+            Contacts: [
+              {
+                ReferenceNumber: flowStatus.parseXml.result.ArchiveData.Egendefinert1.replaceAll(' ', ''),
+                Role: 'Avsender',
+                IsUnofficial: false
+              }
+            ],
+            Files: [
+              {
+                Base64Data: base64,
+                Category: '1',
+                Format: 'pdf',
+                Status: 'F',
+                Title: `Rapportering - den kulturelle spaserstokken - ${xmlData.Egendefinert2}`,
+                VersionFormat: 'A'
+              },
+              ...p360Attachments
+            ],
+            Status: 'J',
+            DocumentDate: new Date().toISOString(),
+            Title: `Rapportering - den kulturelle spaserstokken - ${xmlData.Egendefinert2}`,
+            Archive: 'Saksdokument',
+            CaseNumber: caseNumber,
+            ResponsibleEnterpriseRecno: nodeEnv === 'production' ? '200028' : '200023', // Seksjon Kultur
+            Paragraph: '',
+            AccessGroup: 'Alle'
+          }
+        }
+      }
+    }
+  },
+
+  signOff: {
+    enabled: true
+  },
+
+  closeCase: {
+    enabled: false
+  },
+
+  statistics: {
+    enabled: true,
+    options: {
+      mapper: (flowStatus) => {
+        // Mapping av verdier fra XML-avleveringsfil fra Acos. Alle properties under må fylles ut og ha verdier
+        return {
+          company: 'Samfunnsutvikling',
+          department: 'Kultur',
+          description, // Required. A description of what the statistic element represents
+          type: 'den kulturelle spaserstokken' // Required. A short searchable type-name that distinguishes the statistic element
+          // optional fields:
+        }
+      }
+    }
+  },
+  failOnPurpose: {
+    enabled: false
   }
 }
